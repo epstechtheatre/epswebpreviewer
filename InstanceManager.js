6fb5794794cba5674ec813eb359e31969c617341
Quantum158 <lastisfirst54321@gmail.com>
2021-02-26 23:21:28 -0700

    Final 1

---
+++ InstanceManager.js
@@ -0,0 +1,186 @@
+//Manages all open instances of servers
+const download_repo_git = require("download-git-repo") 
+
+const fs = require("fs")
+const {exec} = require("child_process")
+const Github = require("github-api");
+const auth = require("./auth.json")
+const config = require("./config.json")
+
+var gh = new Github({
+    "token": auth.token
+})
+
+class PRInstance {
+    /**
+     * @type {Object.<string, PRInstance}
+     */
+    static instances = {}
+
+    static GetInstance(PRid) {
+        let output = this.instances[PRid]
+
+        if (output === undefined) {
+            throw Error(`[GetInstance] ${PRid} has no associated instance!`)
+        } else {
+            return output
+        }
+    }
+
+    static prepSiteDirectory() {
+        if (!fs.existsSync("site_instances")) {
+            fs.mkdirSync("site_instances")
+        }
+    }
+
+    /** 
+     * @typedef PRInstanceOptions
+     * @property {Number} PRID ID of the Pull Request
+     * @property {String} SourceRepoFullName Full Name of the Repo merging to the project 
+     * @property {String} Branch Branch name of the repo being merged
+     * @property {String} PRRepoAccount Repo account owner of the project
+     * @property {String} PRRepoName Name of the project
+     */
+
+    /**
+     * 
+     * @param {PRInstanceOptions} options 
+     * @param {"opened"|"edited"|"closed"} [overrideState="opened"] Default opened
+     * @returns {PRInstance} Instance registered in memory and can be recalled with the PR ID
+     */
+    constructor(options) {
+        this.options = options
+
+        PRInstance.instances[this.options.PRID] = this
+
+        return this
+    }
+
+    async download() {
+        //Check if already exists, if so, stop this and swap to edit
+        if (this.#dirExists()) {
+            await this.edit()
+
+        } else {
+            await this.#downloadDir()
+            if (this.activateJekyll()) {
+                await this.comment()
+            }
+        }
+    }
+
+    async edit() {
+        this.killJekyll()
+
+        await this.#deleteDir()
+        await this.#downloadDir()
+        if (this.activateJekyll()) {
+            await this.comment("edit")
+        }
+    }
+
+    async remove() {
+        await this.#deleteDir()
+        this.killJekyll()
+        delete PRInstance.instances[this.options.PRID]
+    }
+
+    async #downloadDir() {
+        let Me = this
+        return new Promise(function (resolve, reject) {
+            download_repo_git(`direct:https://github.com/${Me.options.SourceRepoFullName}/archive/${Me.options.Branch}.zip`, `site_instances/${Me.options.PRID}`, function(err) {
+                if (!err) {
+                    resolve()
+                } else {
+                    reject(err)
+                }
+            })
+        })
+    }
+
+    #deleteDir() {
+        let Me = this
+        return new Promise(function (resolve, reject) {
+            if (!Me.#dirExists()) {
+                resolve()
+            } else {
+                fs.rm(`site_instances/${Me.options.PRID}`, {"recursive": true}, function() {
+                    resolve()
+                })
+            }
+        })
+
+    }
+
+    #dirExists() {
+        if (fs.existsSync(`site_instances/${this.options.PRID}`)) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    activateJekyll() {
+        if (!fs.existsSync(`site_instances/${this.options.PRID}/docs`)) {
+            console.error("[activateJekyll] malformed instance! Skipping...")
+            return false
+        } else {
+            //Set a 6 hour timeout, after this time, close the Jekyll process
+            this.assignedPort = this.#PRidToInt() + config.Starting_Port
+
+            this.process = exec(`bundle exec jekyll serve -P ${(this.assignedPort).toString()}`, {
+                cwd: `site_instances/${this.options.PRID}/docs`,
+                
+            })
+
+            let Me = this
+            this.processTimeout = setTimeout(function() {
+                Me.killJekyll()
+            }, 1000 * 60 * 60 * 6)
+        }
+        return true
+    }
+
+    /**
+     * 
+     * @param {"new"|"edit"} type 
+     */
+    comment(type = "new") {
+        let comment = ""
+        switch (type) {
+            case "new":
+                comment = `Your proposed changes have been downloaded successfully! [Click Here](${config.Domain}:${this.assignedPort} "Click to go to preview site") to preview the wiki with your changes. The preview site will remain active for a six hour period following the most recent update. It will close after this time, or when your changes are merged. Whichever happens first.`
+                break;
+
+            case "edit":
+                comment = `Your latest changes have been downloaded successfully! [Click Here](${config.Domain}:${this.assignedPort} "Click to go to preview site") to preview the wiki with your changes.`
+                break;
+        }
+
+        gh.getIssues(this.options.PRRepoAccount, this.options.PRRepoName).createIssueComment(this.#PRidToInt(), comment, function() {
+            debugger
+        })
+    }
+
+    killJekyll() {
+        this.process?.kill()
+
+        if (this.processTimeout) {
+            clearInterval(this.processTimeout)
+            delete this.processTimeout
+        }
+        //Make sure to clear timeout as well!
+    }
+
+    #PRidToInt() {
+        let output = parseInt(this.options.PRID)
+
+        if (isNaN(output)) {
+            throw Error("[PRidToInt] PRID is not a number!")
+        } else {
+            return output
+        }
+    }
+}
+
+module.exports = PRInstance
--- index.js
+++ index.js
@@ -2,20 +2,102 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 
+const InstanceManager = require("./InstanceManager.js")
+
+//const PR_IDLE_WAIT = 15000 //15 seconds, ensures that latest zip is ready before downloading
+const PR_IDLE_WAIT = 5000 //15 seconds, ensures that latest zip is ready before downloading
+
 // Initialize express and define a port
 const app = express()
-const PORT = 433
+const PORT = 9193
 
 // Tell express to use body-parser's JSON parsing
 app.use(bodyParser.json())
 
 app.post("/hook", (req, res) => {
-  console.log(req.body) // Call your action on the request here
-  res.status(200).end() // Responding is important
-})
 
-//...
+	console.log(`Hook Received!`)
+	//console.log(req.body) // Call your action on the request here
+	res.status(200).end() // Responding is important
+
+	validatePR(req.body)
+})
 
 
 // Start express on the defined port
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+
+app.get("/", (req, res) => {
+    res.status(200).end()
+})
+
+InstanceManager.prepSiteDirectory()
+
+function validatePR(reqBody) {
+	if (reqBody.number) {
+		if (!["opened", "reopened", "synchronize", "closed"].includes(reqBody.action)) {
+			return;
+		}
+
+		//Is now a valid pull request probably
+
+		//Lets wait 15 seconds or so before process, just so that zips of the repo are up to date
+		setTimeout(function() {
+			processPRUpdate(reqBody)
+		}, PR_IDLE_WAIT)
+	}
+}
+
+function processPRUpdate(reqBody) {
+	//Lets figure out what stage the PR is in
+	let branchName = reqBody.pull_request.head.ref
+	let repo = reqBody.pull_request.head.repo.full_name
+	let PRid = reqBody.number
+	let prRepo = reqBody.repository.name
+	let prRepoAuthor = reqBody.repository.owner.login
+
+	switch (reqBody.action) {
+		case "opened":
+		case "reopened":
+			//Build
+			new InstanceManager({
+				"Branch": branchName,
+				"SourceRepoFullName": repo,
+				"PRID": PRid,
+				"PRRepoAccount": prRepoAuthor,
+				"PRRepoName": prRepo
+			}).download()
+			break;
+
+		case "synchronize":
+			try {
+				InstanceManager.GetInstance(PRid).edit()
+			} catch (e) {
+				new InstanceManager({
+					Branch: branchName,
+					PRID: PRid,
+					SourceRepoFullName: repo
+				}).edit()
+			}
+			//Kill, and rebuild
+			break
+
+		case "closed":
+			//Kill and close
+			try {
+				InstanceManager.GetInstance(PRid).remove()
+			} catch (e) {
+				new InstanceManager({
+					Branch: branchName,
+					PRID: PRid,
+					SourceRepoFullName: repo
+				}).remove()
+			}
+			break;
+
+		default:
+			console.log(reqBody.action)
+			//Best to just leave it for now
+			break;
+	}
+}
